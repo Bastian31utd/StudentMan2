27eb6f6be384269f549feabc6efa0664682d5d90
Trinhh <trinhhoanganh.pl@gmail.com>
2024-12-03 08:18:41 +0700

    Add testing project

---
+++ app/src/main/java/com/dxy/studentmanager_2/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.dxy.studentmanager_2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val studentList = mutableListOf("Trinh Hoang Anh - MSSV: 661", "Hoang Van Long - MSSV: 662")
+    }
+}
